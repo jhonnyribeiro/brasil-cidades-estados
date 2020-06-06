235f3bb3908bc944b3605fea0e4047cea6f472bb
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-06-06 06:51:36 -0300

    config log with winston

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log('JS Running...');
 
 import express from 'express';
 import { promises } from 'fs';
+import winston from 'winston';
 
 const readFile = promises.readFile;
 const writeFile = promises.writeFile;
@@ -9,11 +10,30 @@ const writeFile = promises.writeFile;
 const app = express();
 
 app.use(express.json());
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+global.logger = winston.createLogger({
+  level: 'silly',
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'brasil-estados-cidades-api.log' }),
+  ],
+  format: combine(
+    label({ label: 'brasil-estados-cidades-api' }),
+    timestamp(),
+    myFormat
+  ),
+});
 
 app.listen(3000, async () => {
   console.log('API Running...');
+  logger.info('API Running...');
 });
 
 app.get('/test', (req, res) => {
   res.send('API Running... GET Method');
+  logger.info('GET /test - API Running...');
 });
